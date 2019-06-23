f1f0a6caa625fc73b752adbb29b467e690293280
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2019-06-23 21:54:10 +0900

    setup auto-imports for ant

--- backstore-ui/src/component-lib/basic/Button.tsx
+++ backstore-ui/src/component-lib/basic/Button.tsx
@@ -1,1 +1,7 @@
-export const Button = () => null;
+import { Button as AntButton } from 'antd';
+import { NativeButtonProps } from 'antd/lib/button/button';
+import * as React from 'react';
+import styled from 'styled-components';
+export const Button = styled((props: NativeButtonProps) => (
+  <AntButton {...props} />
+))``;
--- backstore-ui/src/components/Root.tsx
+++ backstore-ui/src/components/Root.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
+import { Button } from '../component-lib/basic/Button';
 
 export const Root = () => <div>Hello world!</div>;
