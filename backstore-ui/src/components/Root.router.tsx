b984cd2677c37b4e0d9a407a041181e040ce8638
Stevche Radevski <sradevski@live.com>
2019-08-10 11:43:12 +0200

    Remove store-specific code and move to store-ui

--- backstore-ui/src/components/Root.router.tsx
+++ backstore-ui/src/components/Root.router.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { OnboardingRouter } from './onboarding/Onboarding.router';
 import { DashboardRouter } from './dashboard/Dashboard.router';
-import { StoreRouter } from './store/Store.router';
 
 export interface RootRouterProps {
   hasFinishedOnboarding: boolean;
@@ -13,8 +12,6 @@ export const RootRouter = ({ hasFinishedOnboarding }: RootRouterProps) => {
     <Switch>
       <Route path='/onboarding' component={OnboardingRouter} />
       <Route path='/dashboard' component={DashboardRouter} />
-      <Route path='/store' component={StoreRouter} />
-      {/* <Redirect to='/store' /> */}
       <Redirect to={hasFinishedOnboarding ? '/dashboard' : 'onboarding'} />
     </Switch>
   );
