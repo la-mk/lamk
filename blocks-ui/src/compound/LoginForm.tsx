341259821e9448b1a088edf5dc7405007a34d4d6
Stevche Radevski <sradevski@live.com>
2019-09-20 10:45:27 +0200

    Added link to signup and login pages on the login and signup forms

--- blocks-ui/src/compound/LoginForm.tsx
+++ blocks-ui/src/compound/LoginForm.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Flex, Col, Form, FormItem, formInput, Button, Title } from '..';
+import { Text } from '../basic/Typography';
+import { Divider } from '../basic/Divider';
 
 export interface LoginProps {
   login: (credentials: LoginCredentials) => void;
+  onSignupNowClick: () => void;
   validate: (data: LoginCredentials) => { [key: string]: string };
   validateSingle: (val: any, selector: string) => string;
 }
@@ -12,32 +15,44 @@ export interface LoginCredentials {
   password: string;
 }
 
-export const LoginForm = ({ login, validate, validateSingle }: LoginProps) => {
+export const LoginForm = ({
+  login,
+  onSignupNowClick,
+  validate,
+  validateSingle,
+}: LoginProps) => {
   return (
-    <Col width={['100%', '80%', '60%']}>
-      <Form
-        labelCol={{ span: 6 }}
-        wrapperCol={{ span: 12 }}
-        layout='horizontal'
-        colon={false}
-        onFormCompleted={(data: LoginCredentials) => login(data)}
-        validate={validate}
-        validateSingle={validateSingle}
-      >
-        <FormItem selector='email' label='Email Address'>
-          {formInput()}
-        </FormItem>
+    <>
+      <Col width={['100%', '80%', '60%']}>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 12 }}
+          layout='horizontal'
+          colon={false}
+          onFormCompleted={(data: LoginCredentials) => login(data)}
+          validate={validate}
+          validateSingle={validateSingle}
+        >
+          <FormItem selector='email' label='Email Address'>
+            {formInput()}
+          </FormItem>
 
-        <FormItem selector='password' label='Password'>
-          {formInput({ type: 'password' })}
-        </FormItem>
+          <FormItem selector='password' label='Password'>
+            {formInput({ type: 'password' })}
+          </FormItem>
 
-        <Flex justifyContent='center' alignItems='center'>
-          <Button mr={2} type='primary' htmlType='submit' size='large'>
-            Login
-          </Button>
-        </Flex>
-      </Form>
-    </Col>
+          <Flex justifyContent='center' alignItems='center'>
+            <Button mr={2} type='primary' htmlType='submit' size='large'>
+              Login
+            </Button>
+          </Flex>
+        </Form>
+      </Col>
+      <Divider />
+      <Text type='secondary'>No account?</Text>
+      <Button type='link' onClick={onSignupNowClick}>
+        Sign up now
+      </Button>
+    </>
   );
 };
--- blocks-ui/src/compound/SignupForm.tsx
+++ blocks-ui/src/compound/SignupForm.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Flex, Col, Form, FormItem, formInput, Button, Title } from '..';
+import { Divider } from '../basic/Divider';
+import { Text } from '../basic/Typography';
 
 export interface SignupProps {
   signup: (credentials: SignupCredentials) => void;
+  onLoginNowClick: () => void;
   validate: (data: SignupCredentials) => { [key: string]: string };
   validateSingle: (val: any, selector: string) => string;
 }
@@ -13,34 +16,42 @@ export interface SignupCredentials {
 }
 export const SignupForm = ({
   signup,
+  onLoginNowClick,
   validate,
   validateSingle,
 }: SignupProps) => {
   return (
-    <Col width={['100%', '80%', '60%']}>
-      <Form
-        labelCol={{ span: 6 }}
-        wrapperCol={{ span: 12 }}
-        layout='horizontal'
-        colon={false}
-        onFormCompleted={(data: SignupCredentials) => signup(data)}
-        validate={validate}
-        validateSingle={validateSingle}
-      >
-        <FormItem selector='email' label='Email Address'>
-          {formInput()}
-        </FormItem>
+    <>
+      <Col width={['100%', '80%', '60%']}>
+        <Form
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 12 }}
+          layout='horizontal'
+          colon={false}
+          onFormCompleted={(data: SignupCredentials) => signup(data)}
+          validate={validate}
+          validateSingle={validateSingle}
+        >
+          <FormItem selector='email' label='Email Address'>
+            {formInput()}
+          </FormItem>
 
-        <FormItem selector='password' label='Password'>
-          {formInput({ type: 'password' })}
-        </FormItem>
+          <FormItem selector='password' label='Password'>
+            {formInput({ type: 'password' })}
+          </FormItem>
 
-        <Flex justifyContent='center' alignItems='center'>
-          <Button mr={2} type='primary' htmlType='submit' size='large'>
-            Signup
-          </Button>
-        </Flex>
-      </Form>
-    </Col>
+          <Flex justifyContent='center' alignItems='center'>
+            <Button mr={2} type='primary' htmlType='submit' size='large'>
+              Signup
+            </Button>
+          </Flex>
+        </Form>
+      </Col>
+      <Divider />
+      <Text type='secondary'>Already have an account?</Text>
+      <Button type='link' onClick={onLoginNowClick}>
+        Log in now
+      </Button>
+    </>
   );
 };
