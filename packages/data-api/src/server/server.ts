49be9ac1576a59e5e2937307ee379a7d65c2f839
Stevche Radevski <sradevski@live.com>
2019-12-19 03:01:43 +0100

    remove unused static route from data api

--- packages/data-api/src/server/server.ts
+++ packages/data-api/src/server/server.ts
@@ -14,7 +14,6 @@ import { initPostRouteMiddlewares } from './middlewares/postRouteMiddlewares';
 import { initErrorHandlingMiddlewares } from './middlewares/errorHandlingMiddleware';
 
 import { registerServices } from './routes/services';
-import { registerStaticRoutes } from './routes/static';
 import { registerAppHooks } from './hooks';
 import { registerChannels } from './channels/channels';
 import { initMongoClient } from './mongo';
@@ -42,7 +41,6 @@ export default async () => {
   await initMongoClient(app);
 
   // Register routes (services) and hooks
-  registerStaticRoutes(app);
   registerAppHooks(app);
   registerServices(app);
   registerChannels(app);
