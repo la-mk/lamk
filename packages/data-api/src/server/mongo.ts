f531f0b7260a6fb72ef1d0ea773b6e7d5082cd54
Stevche Radevski <sradevski@live.com>
2020-01-20 19:16:37 +0100

    Add more logging to mongodb init code and specify retry logic

--- packages/data-api/src/server/mongo.ts
+++ packages/data-api/src/server/mongo.ts
@@ -1,13 +1,23 @@
+import { Application } from '@feathersjs/express';
 import { MongoClient } from 'mongodb';
 import env from '../common/env';
-import { Application } from '@feathersjs/express';
+import { logger } from '../common/logger';
 
 export const initMongoClient = async (app: Application) => {
-  const client = await MongoClient.connect(env.MONGODB_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    logger.info('Connecting to MongoDB');
+    const client = await MongoClient.connect(env.MONGODB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      reconnectTries: 10,
+      reconnectInterval: 1000,
+    });
 
-  app.set('mongoClient', client);
-  app.set('mongoDb', client.db(env.MONGODB_DB_NAME));
+    logger.info('Connected to MongoDB');
+    app.set('mongoClient', client);
+    app.set('mongoDb', client.db(env.MONGODB_DB_NAME));
+  } catch (err) {
+    logger.error('Failed to connect to MongoDB');
+    logger.error(err);
+  }
 };
