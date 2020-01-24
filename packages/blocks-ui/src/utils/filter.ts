2c447f518a0b6202fdca821e15b8ea5ae6f33683
Stevche Radevski <sradevski@live.com>
2020-01-24 23:19:18 +0100

    blocks-ui: Add common filters to filter utils

--- packages/blocks-ui/src/utils/filter.ts
+++ packages/blocks-ui/src/utils/filter.ts
@@ -2,6 +2,47 @@ import toInteger from 'lodash/toInteger';
 import * as queryString from 'qs';
 import { FilterObject, MinifiedFilterObject } from '../hooks/useFilter';
 
+export const singleItemFilter = (fieldName: string, value: string | number, notEqual?: boolean) => {
+  if(notEqual) {
+    return {[fieldName]: {$ne: value}}
+  }
+  
+  return {[fieldName]: value}
+}
+
+export const multipleItemsFilter = (fieldName: string, items: Array<string | number>, notEqual?: boolean) => {
+  if (!items.length) {
+    return { [fieldName]: undefined };
+  }
+
+  if (items.length === 1) {
+    return singleItemFilter(fieldName, items[0], notEqual);
+  }
+
+  if(notEqual){
+    return { [fieldName]: { $nin: items } };  
+  }
+  
+  return { [fieldName]: { $in: items } };
+}
+
+export const rangeFilter = (fieldName: string, from: number, to: number, minValue: number, maxValue: number ) => {
+  let query: { $gte?: number; $lte?: number } = {};
+  if (from === to || from > to) {
+    return { [fieldName]: from };
+  }
+
+  if (from > minValue) {
+    query.$gte = from;
+  }
+
+  if (to < maxValue) {
+    query.$lte = to;
+  }
+
+  return { [fieldName]: query };
+}
+
 export const expandFilterObject = (obj: MinifiedFilterObject): FilterObject => {
   if (!obj) {
     return {};
