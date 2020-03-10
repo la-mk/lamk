c6ece1bfe5befbe7c3f7657a178880ec898bd993
Stevche Radevski <sradevski@live.com>
2020-03-10 22:08:03 +0100

    la-sdk: refactor pricing calculation and expose method to get applicable campaigns

--- packages/la-sdk/src/utils/pricing.ts
+++ packages/la-sdk/src/utils/pricing.ts
@@ -9,6 +9,13 @@ export const calculateProductPrice = (product: Pick<Product, 'price' | 'discount
   return product.price - (product.discount ?? 0);
 }
 
+export const calculateProductsTotal = (productsWithQuantity: (CartItemWithProduct | OrderItem)[]) => {
+  return sum(
+    productsWithQuantity.map(
+      ({product, quantity}) => (product.calculatedPrice ?? calculateProductPrice(product)) * (quantity ?? 1)
+    ));
+}
+
 const calculateWithDiscountCampaign = (campaign: Pick<Campaign, 'reward'>, productsTotal: number) => {
   switch(campaign.reward.type){
     case RewardTypes.PERCENTAGE_DISCOUNT: {
@@ -28,14 +35,17 @@ const getBestCampaign = (campaigns: Pick<Campaign, 'reward'>[], productsTotal: n
   }));
 }
 
+export const getApplicableCampaigns = (campaigns: Pick<Campaign, 'reward'>[], productsWithQuantity: (CartItemWithProduct | OrderItem)[]) => {
+  // For now, there can only be a single campaign applied to all products, so we just get the one that has the best reward. Price calculation will be much more complex as other types of campaigns will be supported.
+  const productsTotal = calculateProductsTotal(productsWithQuantity);
+  const bestCampaign = getBestCampaign(campaigns, productsTotal);
+  return bestCampaign ? [bestCampaign] : [];
+}
+
 export const calculatePrices = (productsWithQuantity: (CartItemWithProduct | OrderItem)[], delivery?: Pick<Delivery, 'freeDeliveryOver' | 'price'>, campaigns: Pick<Campaign, 'reward'>[] = []) => {
 
-  const productsTotal = sum(
-    productsWithQuantity.map(
-      ({product, quantity}) => (product.calculatedPrice ?? calculateProductPrice(product)) * (quantity ?? 1)
-    ));
-  
-  const bestCampaign = getBestCampaign(campaigns, productsTotal);
+  const productsTotal = calculateProductsTotal(productsWithQuantity)
+  const bestCampaign = getApplicableCampaigns(campaigns, productsWithQuantity)[0];
   const withCampaignsTotal = bestCampaign ? calculateWithDiscountCampaign(bestCampaign, productsTotal) : productsTotal;
   const deliveryTotal = delivery ? (delivery.freeDeliveryOver < withCampaignsTotal ? 0 : delivery.price) : undefined;
   const total = withCampaignsTotal + (deliveryTotal ?? 0);
