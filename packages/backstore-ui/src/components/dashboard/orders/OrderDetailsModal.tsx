0fdd79af75e5248eaed25b8e4c18b1b5b3c17a67
Stevche Radevski <sradevski@live.com>
2020-07-30 23:39:53 +0200

    backstore-ui: Add product variant support

--- packages/backstore-ui/src/components/dashboard/orders/OrderDetailsModal.tsx
+++ packages/backstore-ui/src/components/dashboard/orders/OrderDetailsModal.tsx
@@ -14,6 +14,7 @@ import {
   Text,
   Divider,
   hooks,
+  Box,
 } from '@sradevski/blocks-ui';
 import { sdk } from '@sradevski/la-sdk';
 import { OrderProduct } from '@sradevski/la-sdk/dist/models/product';
@@ -25,6 +26,7 @@ import { getOrder } from '../../../state/modules/orders/orders.selector';
 import { useTranslation } from 'react-i18next';
 import { getStore } from '../../../state/modules/store/store.selector';
 import { InvoiceDownloadLink } from '../pdfs/invoice/InvoiceDownloadLink';
+import { VariantName } from '../../shared/components/VariantName';
 
 interface OrderDetailsModalProps {
   orderId?: string;
@@ -236,11 +238,29 @@ export const OrderDetailsModal = ({
                         </Flex>
                         <Flex flexDirection='column'>
                           <Text mx={2}>{product.name}</Text>
-                          {product.sku && (
-                            <Text strong mx={2}>
-                              {`${t('product.sku')}: ${product.sku}`}
-                            </Text>
-                          )}
+
+                          <Flex alignItems='center'>
+                            {product.sku && (
+                              <Text strong mx={2}>
+                                {`${t('product.sku')}: ${product.sku}`}
+                              </Text>
+                            )}
+                            {product.attributes && (
+                              <>
+                                <Flex alignItems='center' ml={2}>
+                                  <Text strong>{`${t(
+                                    'product.variant',
+                                  )}: `}</Text>
+                                  <Box ml={1}>
+                                    <VariantName
+                                      t={t}
+                                      attributes={product.attributes}
+                                    />
+                                  </Box>
+                                </Flex>
+                              </>
+                            )}
+                          </Flex>
                         </Flex>
                       </Flex>
                       <Flex mx={2} flexDirection='column'>
---
+++ packages/backstore-ui/src/components/shared/components/VariantName.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Attributes } from '@sradevski/la-sdk/dist/models/product';
+import { Text, Flex, PickerBox } from '@sradevski/blocks-ui';
+import { TFunction } from 'i18next';
+
+export const VariantName = ({
+  attributes,
+  t,
+}: {
+  attributes: Attributes;
+  t: TFunction;
+}) => {
+  const values = Object.entries(attributes ?? {})
+    .filter(([_, val]) => !!val)
+    .map(([key, value], index) => {
+      switch (key) {
+        case 'color': {
+          return (
+            <>
+              {index > 0 && <Text mx={1}>·</Text>}
+              <PickerBox size='small' value={value} type='color' />
+            </>
+          );
+        }
+
+        default:
+          return (
+            <>
+              {index > 0 && <Text mx={1}>·</Text>}
+              {value}
+            </>
+          );
+      }
+    });
+
+  if (!values.length) {
+    return <Text>{t('product.chooseAttributes')}</Text>;
+  }
+
+  return <Flex alignItems='center'>{values}</Flex>;
+};
