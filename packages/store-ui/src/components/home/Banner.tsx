5a6f732591b0b210d2fb95d18e52aaf6fca773eb
Stevche Radevski <sradevski@live.com>
2020-08-01 11:55:01 +0200

    store-ui: Fix banner button transparency

--- packages/store-ui/src/components/home/Banner.tsx
+++ packages/store-ui/src/components/home/Banner.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ImageBackgroundBox } from '../shared/components/ImageBackgroundBox';
 import { sdk } from '@sradevski/la-sdk';
-import { Button, Flex, Title } from '@sradevski/blocks-ui';
+import { Button, Flex, Title, Box } from '@sradevski/blocks-ui';
 import { Store } from '@sradevski/la-sdk/dist/models/store';
 import { useTranslation } from '../../common/i18n';
 import Link from 'next/link';
@@ -31,35 +31,46 @@ export const Banner = ({
         style={{ position: 'relative' }}
       >
         {store.slogan && !hideSlogan && (
-          <Flex
-            style={{
-              position: 'absolute',
-              top: '20%',
-              bottom: '20%',
-              opacity: 0.85,
-            }}
-            width={['80%', '70%', '60%']}
-            ml={['10%', '15%', '20%']}
-            borderRadius={0}
-            bg='background.light'
-            alignItems='center'
-            justifyContent='center'
-            flexDirection='column'
-          >
-            <Title
-              px={[2, 3, 4]}
-              textAlign='center'
-              level={1}
-              fontSize={[5, 6, 6]}
+          <>
+            <Flex
+              style={{
+                position: 'absolute',
+                top: '20%',
+                bottom: '20%',
+                opacity: 0.85,
+              }}
+              width={['80%', '70%', '60%']}
+              ml={['10%', '15%', '20%']}
+              borderRadius={0}
+              bg='background.light'
+            />
+            <Flex
+              style={{
+                position: 'absolute',
+                top: '20%',
+                bottom: '20%',
+              }}
+              width={['80%', '70%', '60%']}
+              ml={['10%', '15%', '20%']}
+              alignItems='center'
+              justifyContent='center'
+              flexDirection='column'
             >
-              {store.slogan}
-            </Title>
-            <Link href='/products' passHref>
-              <Button size='large' type='primary'>
-                {t('actions.shopNow')}
-              </Button>
-            </Link>
-          </Flex>
+              <Title
+                px={[2, 3, 4]}
+                textAlign='center'
+                level={1}
+                fontSize={[5, 6, 6]}
+              >
+                {store.slogan}
+              </Title>
+              <Link href='/products' passHref>
+                <Button size='large' type='primary'>
+                  {t('actions.shopNow')}
+                </Button>
+              </Link>
+            </Flex>
+          </>
         )}
       </ImageBackgroundBox>
     </Flex>
