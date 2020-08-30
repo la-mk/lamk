ed6a287cc98ced71d4052fa5f90a006a6f3915b4
Stevche Radevski <sradevski@live.com>
2020-08-30 10:42:06 +0200

    data-api: Improve logged data for data api

--- packages/data-api/src/common/hooks/log.ts
+++ packages/data-api/src/common/hooks/log.ts
@@ -3,23 +3,26 @@ import { logger } from '../logger';
 import { HookContext } from '@feathersjs/feathers';
 import env from '../env';
 
+const getLogObject = (context: HookContext) => {
+  return `
+  ${context.type} app.service('${context.path}').${context.method}()
+
+  Params: ${JSON.stringify(context.params.query, null, 2)}, ID: ${context.id}, 
+  Data: ${JSON.stringify(_.omit(context.data, 'uri'), null, 2)},
+  ${
+    context.result
+      ? `Result: ${JSON.stringify(_.omit(context.result, 'uri'), null, 2)}`
+      : ''
+  }
+  `;
+};
+
 export const log = (context: HookContext) => {
   if (env().NODE_ENV === 'development') {
-    logger.info(
-      `${context.type} app.service('${context.path}').${context.method}()`,
-    );
-
-    logger.info(
-      `
-      Params: ${JSON.stringify(context.params.query, null, 2)}, ID: ${
-        context.id
-      }, 
-      Data: ${JSON.stringify(_.omit(context.data, 'uri'), null, 2)}, 
-      Result: ${JSON.stringify(_.omit(context.result, 'uri'), null, 2)}`,
-    );
+    logger.info(getLogObject(context));
   }
 
   if (context.error && !context.result) {
-    logger.error(context.error.stack);
+    logger.error(getLogObject(context), context.error.stack);
   }
 };
