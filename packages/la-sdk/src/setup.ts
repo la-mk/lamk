e78b21376672f03824b76e09b1d291811ef0b2db
Stevche Radevski <sradevski@live.com>
2020-10-02 14:34:08 +0200

    la-sdk: Increase the timeout to 8 seconds

--- packages/la-sdk/src/setup.ts
+++ packages/la-sdk/src/setup.ts
@@ -34,6 +34,7 @@ export const setupClient = (options: SetupSdkOptions) => {
     }
   } else {
     const socket = io(host, {
+      timeout: 8000,
       // This is needed if there is a cluster of services running, check https://docs.feathersjs.com/cookbook/general/scaling.html#cluster-configuration.
       // transports: ['websocket'],
       // upgrade: false,
