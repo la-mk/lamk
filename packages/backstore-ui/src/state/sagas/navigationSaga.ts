b77f081f392a6b698bb2b031aa1d7a295e07e065
Stevche Radevski <sradevski@live.com>
2020-10-17 18:17:39 +0200

    backstore-ui: Fix onboarding redirect and release new version

--- packages/backstore-ui/src/state/sagas/navigationSaga.ts
+++ packages/backstore-ui/src/state/sagas/navigationSaga.ts
@@ -15,15 +15,17 @@ import { setUiReady } from '../modules/ui/ui.module';
 function* storeStateSaga(action: LocationChangeAction) {
   const store = yield select(getStore);
   const user = yield select(getUser);
-  const isPathOnboarding = document.location.pathname.includes('/onboarding');
 
   if (user && !store) {
     const stores = yield call(sdk.store.findOwned, user._id);
+    const isPathOnboarding = document.location.pathname.includes('/onboarding');
+    const isPathDashboard = document.location.pathname.includes('/dashboard');
 
     if (stores.total > 0) {
       yield put(setStore(stores.data[0]));
 
-      if (isPathOnboarding) {
+      // Basically if it is a login, signup, reset password page, but they are logged in already, redirect them to the dashboard.
+      if (!isPathOnboarding && !isPathDashboard) {
         yield put(replaceTo('/dashboard'));
       }
     } else if (!isPathOnboarding) {
