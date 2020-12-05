21a53bf3d5e7b099b26db18bc3c15f821f57a948
Stevche Radevski <sradevski@live.com>
2020-12-05 12:33:13 +0100

    blocks-ui: Small fixes to text component

--- packages/blocks-ui/src/basic/Text/index.tsx
+++ packages/blocks-ui/src/basic/Text/index.tsx
@@ -9,11 +9,22 @@ import { TextSize } from '../../system';
 export interface TextProps
   extends Pick<
       ChakraTextProps,
-      'children' | 'isTruncated' | 'noOfLines' | 'align' | 'casing' | 'color'
+      | 'children'
+      | 'isTruncated'
+      | 'noOfLines'
+      | 'align'
+      | 'casing'
+      | 'color'
+      | 'maxWidth'
+      | 'minWidth'
     >,
     SpaceProps {
   size?: TextSize;
   as?: As;
 }
+
+ChakraText.defaultProps = {
+  as: 'span',
+};
 
 export const Text = ChakraText as React.FunctionComponent<TextProps>;
