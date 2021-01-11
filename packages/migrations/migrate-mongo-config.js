51ad72def4bbe8f0050bab1f8331fd58d085e3a4
Stevche Radevski <sradevski@live.com>
2021-01-11 21:29:10 +0100

    migrations: Add migration for images as an object

--- packages/migrations/migrate-mongo-config.js
+++ packages/migrations/migrate-mongo-config.js
@@ -8,13 +8,13 @@ const config = {
       useUnifiedTopology: true, // removes a deprecating warning when connecting
       //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
       //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
-    }
+    },
   },
 
   // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
   migrationsDir: "migrations",
   // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
-  changelogCollectionName: "changelog"
+  changelogCollectionName: "changelog",
 };
 
 // Return the config as a promise
---
+++ packages/migrations/migrations/20210111173450-image-as-object.js
@@ -0,0 +1,86 @@
+const Bluebird = require("bluebird");
+
+module.exports = {
+  async up(db) {
+    const products = await db.collection("products").find({}).toArray();
+    await Bluebird.map(
+      products,
+      (product) => {
+        const media = (product.images || []).map((image) => ({
+          _id: image,
+          height: 1000,
+          width: 1000,
+          size: 100,
+          mimeType: "image/png",
+        }));
+
+        console.log(`Updating product: ${product.name}`);
+        return db
+          .collection("products")
+          .updateMany(
+            { _id: product._id },
+            { $set: { media }, $unset: { images: "" } }
+          );
+      },
+      { concurrency: 30 }
+    );
+
+    const stores = await db.collection("stores").find({}).toArray();
+    await Bluebird.map(
+      stores,
+      (store) => {
+        if (!store.logo) {
+          return;
+        }
+
+        console.log(`Updating store: ${store.slug}`);
+        const logo = {
+          _id: store.logo,
+          height: 1000,
+          width: 1000,
+          size: 100,
+          mimeType: "image/png",
+        };
+
+        return db
+          .collection("stores")
+          .updateMany({ _id: store._id }, { $set: { logo } });
+      },
+      { concurrency: 30 }
+    );
+
+    const storeContents = await db
+      .collection("storeContents")
+      .find({})
+      .toArray();
+    await Bluebird.map(
+      storeContents,
+      (storeContent) => {
+        if (!storeContent.landing || !storeContent.landing.banner) {
+          return;
+        }
+
+        const banner = {
+          _id: storeContent.landing.banner,
+          height: 1000,
+          width: 1000,
+          size: 100,
+          mimeType: "image/png",
+        };
+
+        console.log(`Updating store content: ${storeContent._id}`);
+        return db
+          .collection("storeContents")
+          .updateMany(
+            { _id: storeContent._id },
+            { $set: { "landing.banner": banner } }
+          );
+      },
+      { concurrency: 30 }
+    );
+  },
+
+  async down(db, client) {
+    // Just revert from the DB snapshot
+  },
+};
