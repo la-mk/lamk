aa5f76711b12abc679b57090db2fa0257acbdbaa
Stevche Radevski <sradevski@live.com>
2021-01-21 21:46:04 +0100

    blocks-ui: Add custom components for markdown rendering

--- packages/blocks-ui/src/basic/Markdown/index.tsx
+++ packages/blocks-ui/src/basic/Markdown/index.tsx
@@ -5,6 +5,10 @@ import rehype from 'remark-rehype';
 import react from 'rehype-react';
 import { Divider } from '../Divider';
 import { Text } from '../Text';
+import { Heading } from '../Heading';
+import { Button } from '../Button';
+import { Image } from '../Image/Image';
+import { Flex } from '../Flex';
 
 export const getProcessor = () => {
   let processor = unified()
@@ -13,15 +17,57 @@ export const getProcessor = () => {
     .use(react, {
       createElement: React.createElement,
       components: {
-        p: (props: any) => <Text {...props} />,
-        // a: (props: any) => <Link {...props} />,
-        // h1: (props: any) => <Heading.h1 {...props} />,
-        // h2: (props: any) => <Heading.h2 {...props} />,
-        // h3: (props: any) => <Heading.h3 {...props} />,
-        // h4: (props: any) => <Heading.h4 {...props} />,
-        // h5: (props: any) => <Heading.h5 {...props} />,
-        // h6: (props: any) => <Heading.h6 {...props} />,
-        hr: (props: any) => <Divider {...props} />,
+        p: (props: any) => <Text as="p" {...props} />,
+        b: (props: any) => <Text as="b" {...props} />,
+        i: (props: any) => <Text as="i" {...props} />,
+        blockquote: (props: any) => <Text as="blockquote" {...props} />,
+        code: (props: any) => <Text as="code" {...props} />,
+        a: (props: any) => <Button as="a" variant="link" {...props} />,
+        img: (props: any) => {
+          // TODO: Find a better way to detect if it is a video from markdown
+          const isVideo = props.src.includes('youtube.com');
+          if (isVideo) {
+            return (
+              <Flex
+                width="100%"
+                // @ts-ignore
+                position="relative"
+                overflow="hidden"
+                pt="56.25%"
+                align="center"
+                justify="center"
+              >
+                <iframe
+                  style={{ position: 'absolute', top: 0, left: 0 }}
+                  height="100%"
+                  width="100%"
+                  src={props.src}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                />
+              </Flex>
+            );
+          }
+          return (
+            <Flex align="center" justify="center" my={3}>
+              <Image {...props} />
+            </Flex>
+          );
+        },
+        h1: (props: any) => (
+          <Heading as="h1" size="3xl" mb={5} mt={6} {...props} />
+        ),
+        h2: (props: any) => (
+          <Heading as="h2" size="2xl" mb={5} mt={6} {...props} />
+        ),
+        h3: (props: any) => (
+          <Heading as="h3" size="xl" mb={4} mt={5} {...props} />
+        ),
+        h4: (props: any) => (
+          <Heading as="h4" size="lg" mb={4} mt={5} {...props} />
+        ),
+        hr: (props: any) => <Divider my={6} {...props} />,
       },
     });
 
--- packages/blocks-ui/src/basic/Markdown/story.tsx
+++ packages/blocks-ui/src/basic/Markdown/story.tsx
@@ -5,10 +5,42 @@ import { Provider } from '../../';
 
 storiesOf('Markdown', module).add('standard', () => {
   const test = `
-  ### This is a test
+  # First title
+  ## Second title
+  ### Third title
+  #### Fourth title
   _______
 
+  **bold text**
+
+  *italicized text*
+  > blockquote
+
+  &nbsp;
+
+  1. First item
+  2. Second item
+  3. Third item
+
+  - First item
+  - Second item
+  - Third item
+
+
+  \`code\`
+
+
+  [Duck Duck Go](https://duckduckgo.com)
+
+
+  <fake@example.com>
+  
+  ![alt text for image](https://picsum.photos/300/300)
+  ![alt text for video  ](https://www.youtube.com/embed/Hsd5k8mWdV0)
+
   Hey there
+
+
   `;
 
   return (
