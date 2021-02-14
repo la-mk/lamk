df2adce8c8fd4fb4aab73362526f634fd40f4b93
Stevche Radevski <sradevski@live.com>
2021-02-14 11:37:50 +0100

    landing-ui: Add date-fns and format blog date

--- packages/landing-ui/src/blog/Timestamp.tsx
+++ packages/landing-ui/src/blog/Timestamp.tsx
@@ -1,14 +1,20 @@
 import { Text } from '@la-mk/blocks-ui';
 import React from 'react';
+import { format } from 'date-fns';
+import { mk, enUS } from 'date-fns/locale';
+import { useTranslation } from '../common/i18n';
 
 export const Timestamp = ({ timestamp }: { timestamp: string | number }) => {
+  const { i18n } = useTranslation();
+
   return (
     <Text size='sm' color='mutedText.dark'>
-      {new Intl.DateTimeFormat('mk-MK', {
-        // @ts-ignore
-        dateStyle: 'full',
-        timeStyle: 'short',
-      }).format(new Date(timestamp))}
+      {
+        // TODO: Handle locales better
+        format(new Date(timestamp), 'PPPP', {
+          locale: i18n.language === 'mk' ? mk : enUS,
+        })
+      }
     </Text>
   );
 };
