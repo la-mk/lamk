e63e472941978ecc22379ee82d3680fd7068b7c2
sradevski <sradevski@live.com>
2021-04-18 17:56:46 +0200

    la-sdk: Fix timeout not being set correctly

--- packages/la-sdk/src/setup.ts
+++ packages/la-sdk/src/setup.ts
@@ -34,12 +34,11 @@ export const setupClient = (options: SetupSdkOptions) => {
     }
   } else {
     const socket = io(host, {
-      timeout: 15000,
       // This is needed if there is a cluster of services running, check https://docs.feathersjs.com/cookbook/general/scaling.html#cluster-configuration.
       // transports: ['websocket'],
       // upgrade: false,
     });
-    client.configure(socketio(socket));
+    client.configure(socketio(socket, { timeout: 15000 }));
   }
 
   client.configure(
