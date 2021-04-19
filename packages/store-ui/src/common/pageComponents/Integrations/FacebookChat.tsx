f2e184c1703b5a99d59a7f3698fd4f3eb11b0603
sradevski <sradevski@live.com>
2021-04-19 23:53:31 +0200

    store-ui: Fix how facebook chat is loaded and release new version

--- packages/store-ui/src/common/pageComponents/Integrations/FacebookChat.tsx
+++ packages/store-ui/src/common/pageComponents/Integrations/FacebookChat.tsx
@@ -1,34 +1,44 @@
+import { useEffect } from 'react';
+
 export const FacebookChat = ({ pageId }: { pageId: string }) => {
-  return (
-    <>
-      <div id='fb-root'></div>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.fbAsyncInit = function() {
-              FB.init({
-                xfbml            : true,
-                version          : 'v10.0'
-              });
-            };
+  useEffect(() => {
+    document.getElementById('fb-root')?.remove();
+    document.getElementsByClassName('fb-customerchat')[0]?.remove();
+    document.getElementById('fb-chat-script')?.remove();
 
-            (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-            `,
-        }}
-      />
+    const r = document.createElement('div');
+    r.id = 'fb-root';
 
-      <div
-        className='fb-customerchat'
-        // @ts-ignore
-        attribution='biz_inbox'
-        page_id={pageId}
-      ></div>
-    </>
-  );
+    const c = document.createElement('div');
+    c.className = 'fb-customerchat';
+    c.setAttribute('attribution', 'biz_inbox');
+    c.setAttribute('data-page_id', pageId);
+
+    const s = document.createElement('script');
+    s.id = 'fb-chat-script';
+    s.type = 'text/javascript';
+    s.innerHTML = `
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml            : true,
+        version          : 'v10.0'
+      });
+    };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    `;
+
+    const body = document.getElementsByTagName('body')[0];
+    body.appendChild(r);
+    body.appendChild(s);
+    body.appendChild(c);
+  }, [pageId]);
+
+  return <></>;
 };
