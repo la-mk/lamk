ccc33bb9c5a43d1713c971a85fd41436b2376762
Stevche Radevski <sradevski@live.com>
2022-05-22 16:16:19 +0200

    fix the locale redirects

--- packages/store-ui-new/next-i18next.config.js
+++ packages/store-ui-new/next-i18next.config.js
@@ -3,7 +3,9 @@ const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "mk",
-    locales: ["en", "mk"],
+    locales: ["mk", "en"],
+    // This prevents nextjs from redirecting based on Accept-Language header
+    localeDetection: false,
   },
   defaultNS: "translation",
   localePath: path.resolve("./public/locales"),
